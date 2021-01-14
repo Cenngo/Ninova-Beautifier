d37cb2ec8cecde733298ac4741f6f943062d6970
Cenk <cenk.ergen1@gmail.com>
2021-01-15 00:55:19 +0300

    Css variables can now be changed by the user

--- style.js
+++ style.js
@@ -1,3 +1,16 @@
-chrome.storage.local.get(['bgImg'], function(result){
-    document.getElementsByTagName('body')[0].style.setProperty("background-image", `url("${result.bgImg}")`, 'important');
-});
+window.addEventListener("load", load);
+
+function load(window, ev){
+    chrome.storage.local.get(['bgImg', 'opacity', 'hlColor', 'mainColor', 'textColor', 'headerColor', 'accent', 'accentS'], (result) =>{
+        document.getElementsByTagName('body')[0].style.setProperty("background-image", `url("${result.bgImg}")`, 'important');
+        const style = document.documentElement.style;
+        
+        style.setProperty("--mainColor", result.mainColor);
+        style.setProperty("--textColor", result.textColor);
+        style.setProperty("--hyperlinkColor", result.hlColor);
+        style.setProperty("--opacity", result.opacity / 100);
+        style.setProperty("--headerColor", result.headerColor);
+        style.setProperty("--accent", result.accent);
+        style.setProperty("--secondary", result.accentS);
+    });
+};
