42237b4cb923e4e351eb333bfdea1192e70cbb8a
Cenk <cenk.ergen1@gmail.com>
2021-03-10 16:16:42 +0300

    collected constants in a file

---
+++ enums.js
@@ -0,0 +1,6 @@
+const storedVariables = ['bgImg', 'opacity', 'bgColor', 'hlColor', 'mainColor', 'lessonColor', 'textColor', 'headerColor', 'accent', 
+    'accentS', 'showLogo', 'showCaptions', 'unlockZoom'];
+const urlPatterns = {
+    ninova: "https://ninova.itu.edu.tr/*"
+};
+const extensionName = "Ninova Beautifier";
