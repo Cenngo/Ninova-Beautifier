a7cbf5df08b5701d0275e9b949fa17cccb888990
Cenk <cenk.ergen1@gmail.com>
2021-03-18 12:29:43 +0300

    chagend captions styling to straight font

--- captions.js
+++ captions.js
@@ -2,7 +2,7 @@ Array.from(window["Tips"].tips).forEach(tip => {
     var element = tip.element;
     if(element.innerText.match(/[A-Z]{3} [0-9]{3}/gm)){
         var caption = tip.content.replace(/<br>.*/gm, "").replace(/<\/?[^>]+(>|$)/g, "");
-        var title = document.createElement('i');
+        var title = document.createElement('p');
         title.innerText = caption + " - ";
         element.prepend(title);
     }
